54eea33f7f4c6a3e9ca610ff8bd2aa450036fe32
yunpengliu <liuyp@sugon.com>
2025-11-20 11:50:10 +0800

    release v0.0.3

--- packages/desktop/src-tauri/src/main.rs
+++ packages/desktop/src-tauri/src/main.rs
@@ -1,20 +1,5 @@
-// packages/desktop/src-tauri/src/main.rs
-
-use tauri::Manager; // 引入 Manager trait
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .setup(|app| {
-            #[cfg(debug_assertions)] // 开发模式
-            {
-                let window = app.get_webview_window("main").unwrap();
-                window.open_devtools();
-            }
-            // 如果你想在打出来的包里也能按 F12 看报错，把下面的注释解开：
-            let window = app.get_webview_window("main").unwrap();
-            window.open_devtools();
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    tada_desktop_lib::run()
 }
