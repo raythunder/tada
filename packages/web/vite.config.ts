cc3f1d3a1a3cceda70ac253ed2354b12a9a86637
caizb <caizhibin@cleartv.cn>
2026-01-28 17:16:32 +0800

    feat: 引入 Caddy 作为反向代理和静态文件服务，并优化 Docker Compose 配置以支持生产环境部署

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-    base: '/tada/',
+    base: '/',
     plugins: [react()],
 
     css: {
